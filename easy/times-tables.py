e643ee86c7c942c349473456466bb2b92250ce6b
Thomas <thomas.louis.hennessy@gmail.com>
2020-10-23 14:26:49 +0100

    added times table

---
+++ easy/times-tables.py
@@ -0,0 +1,5 @@
+x = -10
+while x < 90:
+    x += 10
+
+    print(str(x+1) + ", " + str(x+2) + ", " + str(x+3) + ", " + str(x+4) + ", " + str(x+5) + ", " + str(x+6) + ", " + str(x+7) + ", " + str(x+8) + ", " + str(x+9) + ", " + str(x+10))
