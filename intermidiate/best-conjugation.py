4ba88e60d41f545ccb5588dda2f588d67de062fb
Thomas <thomas.louis.hennessy@gmail.com>
2020-10-26 16:06:45 +0000

    best conjugation still in progress

--- intermidiate/best-conjugation.py
+++ intermidiate/best-conjugation.py
@@ -1,11 +1,18 @@
 import requests
 
+# def compare_words(first_word, second_word):
+#     if first_word == second_word:
+#         return True
+#     else:
+#         return False
+
+
 given_word = input("Enter your word:\n")
 
 obtained_words = requests.get("http://www-personal.umich.edu/~jlawler/wordlist")
-# print(obtained_words.text[100])
 
 conjugated_list = []
+current_search_word = ""
 
 count_left = 0
 count_right = 1
@@ -18,21 +25,24 @@ while count_left < len(given_word):
         # using the counters to slice a word
         current_word = given_word[count_left:count_right]
 
-        print("counter left :", count_left, ",counter right:", count_right, ",word produced", current_word)
-
-        # disallows the full word to be a subword
-        if current_word != given_word:
-            # if the word being searched is found in the list of requested words from earlier
-            # add the word to the conjugated list
-            for search_word in obtained_words.text:
-                if search_word == current_word:
-                    print("Success on", search_word)
+        # if the word being searched is found in the list of requested words from earlier
+        # add the word to the conjugated list
+        for search_letter in obtained_words.text:
+            # responce objects are a list of chars, so the words need to be reconstituted
+            if search_letter != "\n" or search_letter != " " or search_letter != "  ":
+                current_search_word = current_search_word + search_letter
+                print("unfinished word",current_search_word)
+            else:
+                print("searching using",current_search_word)
+                if current_search_word == current_word:
                     conjugated_list.append(current_word)
+                    del current_search_word
                     break
 
+        # increments the right counter
         count_right += 1
 
-    
+    # increments the left counter to start the process again and moves the right counter to one position to the right of the left counter
     count_left += 1
     count_right = count_left + 1
 
